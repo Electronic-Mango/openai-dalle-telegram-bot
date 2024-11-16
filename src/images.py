937eb7b0b7afcb960b100c6143937d9289a817d4
Electronic Mango <78230210+Electronic-Mango@users.noreply.github.com>
2024-11-16 11:09:24 +0100

    Handler errors during image generation

--- src/images.py
+++ src/images.py
@@ -1,7 +1,7 @@
 from os import getenv
 
 from dotenv import load_dotenv
-from openai import AsyncOpenAI
+from openai import AsyncOpenAI, OpenAIError
 
 load_dotenv()
 
@@ -12,11 +12,14 @@ client = AsyncOpenAI(api_key=TOKEN)
 
 
 async def generate_image(prompt: str) -> str | None:
-    response = await client.images.generate(
-        model=MODEL,
-        prompt=prompt,
-        size="1024x1024",
-        quality="standard",
-        n=1,
-    )
-    return response.data[0].url
+    try:
+        response = await client.images.generate(
+            model=MODEL,
+            prompt=prompt,
+            size="1024x1024",
+            quality="standard",
+            n=1,
+        )
+        return response.data[0].url
+    except OpenAIError as error:
+        return str(error)
